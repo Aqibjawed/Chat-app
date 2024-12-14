f10edb0212860df3d32f0d254633254a7c6b3fd4
Aqibjawed <mohammedaqibjawed@gmail.com>
2024-12-15 01:12:54 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,7 @@
 import React, { useState } from 'react'
-import HomeSidebar from '../components/Sidebar/HomeSidebar'
+import HomeSidebar from '../components/sidebar/HomeSidebar'
 import MessageContainer from '../components/messages/MessageContainer'
-import { useDisplayContext } from '../context/DisplayContext'
 import useConversation from '../zustand/useConversation'
-
-// const Home = () => {
-//   return (
-//     <div className='p-0.5 rounded-lg border-2 border-black border-opacity-60'>
-//         <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-//             <HomeSidebar />
-//             <MessageContainer />
-//         </div>
-//     </div>
-//   )
-// }
 
 
 const Home = () => {
